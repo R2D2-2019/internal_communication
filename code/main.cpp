bce1cb2db347ab0f28188b5377d7d1eed5fdea97
Lex Ruesink <lex.ruesink@student.hu.nl>
2019-03-15 13:47:58 +0100

    Cleanup

--- code/main.cpp
+++ code/main.cpp
@@ -2,19 +2,19 @@
 
 #include <comm.hpp>
 
-struct packet_distance_s {
-    uint16_t mm;
-};
+int main() {
+    using namespace r2d2;
 
-int main(void) {
-  // kill the watchdog
+    // kill the watchdog
     WDT->WDT_MR = WDT_MR_WDDIS;
-    
+
     hwlib::wait_ms(10);
 
-    r2d2::comm_c comm({
-        r2d2::packet_type::GET_DISTANCE
-    });
+    r2d2::comm_c comm(
+        {
+            r2d2::packet_type::GET_DISTANCE
+        }
+    );
 
     packet_distance_s distance;
     distance.mm = 404;
@@ -22,19 +22,21 @@ int main(void) {
     for (;;) {
         hwlib::cout << "Send frame\r\n";
 
-        comm.send(distance, r2d2::can_bus::priority::HIGH);
+        comm.send(distance, priority::HIGH);
         comm.send(distance);
 
         while (comm.has_data()) {
             auto frame = comm.get_data();
 
             switch (frame.type) {
-                case r2d2::packet_type::GET_DISTANCE: {
-                    const auto received = frame.as_packet_type<r2d2::packet_type::GET_DISTANCE>();
+                case packet_type::GET_DISTANCE: {
+                    const auto received = frame.as_packet_type<packet_type::GET_DISTANCE>();
                     hwlib::cout << "Received distance: " << received.mm << "\r\n";
-                } break;
+                }
+                    break;
 
-                default: break;
+                default:
+                    break;
             }
         }
 
