066e994fd0770d9766ab8de988fc3977703aa04a
Lex Ruesink <lex.ruesink@student.hu.nl>
2019-03-15 17:56:28 +0100

    This was a stack. Now it's actually a queue.

--- code/headers/queue.hpp
+++ code/headers/queue.hpp
@@ -29,9 +29,15 @@ namespace r2d2 {
          * Pop an item from the queue.
          */
         void pop() {
-            if (index > 0) {
-                index -= 1;
+            if (empty()) {
+                return;
             }
+
+            for (size_t i = 1; i < index; i++) {
+                buffer[i - 1] = buffer[i];
+            }
+
+            index -= 1;
         }
 
         /**
@@ -39,13 +45,23 @@ namespace r2d2 {
          * and pop.
          */
         T copy_and_pop() {
-            return buffer[--index];
+            T item = front();
+            pop();
+
+            return item;
         }
 
         /**
-         * Get the next item on the queue.
+         * Get the next in the queue.
          */
-        void back() const {
+        T front() const {
+            return buffer[0];
+        }
+
+        /**
+         * Get the last item in the queue.
+         */
+        T back() const {
             return buffer[index - 1];
         }
 
