7e044830c411d2780c6baa3a568d131d8b8b5222
Lex Ruesink <lex.ruesink@student.hu.nl>
2019-03-25 20:05:35 +0100

    Use binary search instead of linear search

--- code/headers/base_comm.hpp
+++ code/headers/base_comm.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <cstring>
 #include <array>
 #include <type_traits>
@@ -134,6 +135,12 @@ namespace r2d2 {
          */
         void listen_for_frames(const std::array<frame_id, 8> listen_for) {
             this->listen_for = listen_for;
+
+            // Sort to enable binary search
+            std::sort(
+                this->listen_for.begin(),
+                this->listen_for.end()
+            );
         }
 
         /**
@@ -182,13 +189,11 @@ namespace r2d2 {
          * @return
          */
         bool accepts_frame(const frame_type &p) const {
-            for (const auto &packet : get_accepted_frame_types()) {
-                if (packet == p) {
-                    return true;
-                }
-            }
+            const auto frames = get_accepted_frame_types();
 
-            return false;
+            return std::binary_search(
+                frames.begin(), frames.end(), p
+            );
         }
     };
 }
