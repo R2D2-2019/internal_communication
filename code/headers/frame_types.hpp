361ebb94ff9f4b5d1882ec63e79cb5ff3ab5b94c
Lex Ruesink <lex.ruesink@student.hu.nl>
2019-05-16 18:22:59 +0200

    Added member offset struct

--- code/headers/frame_types.hpp
+++ code/headers/frame_types.hpp
@@ -1,6 +1,7 @@
 
 #pragma once
 
+#include <cstddef>
 #include <cstdint>
 #include <type_traits>
 
@@ -14,9 +15,14 @@
  * is an exception. It is possible there will be another exception for 
  * the robos instructions later.
  */
-#define R2D2_OPTIMISE_STRING(Type) \
+#define R2D2_OPTIMISE_STRING(Type, MemberName) \
     template<> \
-    struct supports_string_optimisation<Type> : std::true_type {};
+    struct supports_string_optimisation<Type> : std::true_type {}; \
+    \
+    template<> \
+    struct string_member_offset<Type> { \
+        constexpr static uint16_t offset = offsetof(Type, MemberName); \
+    };
 
 
 #define R2D2_INTERNAL_FRAME_HELPER(Type, EnumVal, ...) \
@@ -139,6 +145,17 @@ namespace r2d2 {
     struct supports_string_optimisation : std::false_type {};
     
     /**
+     * Struct that stores the offset of
+     * the string member that can be optimised against.
+     * 
+     * @tparam T
+     */ 
+    template<typename T>
+    struct string_member_offset {
+        constexpr static uint16_t offset = 0;
+    };
+
+    /**
      * Helper accessor to check for string
      * optimisation support on the given type.
      *
@@ -146,6 +163,15 @@ namespace r2d2 {
      */  
     template<typename T>
     constexpr bool supports_string_optimisation_v = supports_string_optimisation<T>::value;
+
+    /**
+     * Helper accessor to get the string member
+     * offset for the given type.
+     * 
+     * @tparam T
+     */ 
+    template<typename T>
+    constexpr bool string_member_offset_v = string_member_offset<T>::offset;
 
     /**
     * A struct that helps to describe
@@ -307,7 +333,7 @@ namespace r2d2 {
     R2D2_INTERNAL_FRAME_HELPER(frame_display_filled_rectangle_s, DISPLAY_FILLED_RECTANGLE)
     R2D2_INTERNAL_FRAME_HELPER(frame_battery_level_s, BATTERY_LEVEL)
     R2D2_INTERNAL_FRAME_HELPER(frame_ui_command_s, UI_COMMAND)
-    R2D2_INTERNAL_FRAME_HELPER(frame_movement_control_s, MOVEMENT_CONTROL)
+    //R2D2_INTERNAL_FRAME_HELPER(frame_movement_control_s, MOVEMENT_CONTROL)
 
     // EXAMPLE: for string optimalisation, the frame doesn't actually support it
     R2D2_INTERNAL_FRAME_HELPER(
@@ -315,6 +341,6 @@ namespace r2d2 {
         MOVEMENT_CONTROL,
 
         // Any extra tags here
-        R2D2_OPTIMISE_STRING(frame_movement_control_s)
+        R2D2_OPTIMISE_STRING(frame_movement_control_s, brake)
     )        
 }
